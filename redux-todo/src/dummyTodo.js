0783bf42c371f4d2d657cc4325b7a92aa79ea8e2
Hoxtygen <hoxtygen@live.com>
2019-06-18 13:03:22 +0100

    Add dummy todos

---
+++ redux-todo/src/dummyTodo.js
@@ -0,0 +1,35 @@
+import uuid from "uuid";
+
+const todos = [
+    {
+        id: uuid(),
+        value: "Make a visit to Anfield",
+        completed: false,
+    },
+
+    {
+        id: uuid(),
+        value: "Transfer Paul Pogba to Leeds FC",
+        completed: false,
+    },
+
+    {
+        id: uuid(),
+        value: "Force the American continent to give Copa America to Argentina",
+        completed: false,
+    },
+
+    {
+        id: uuid(),
+        value: "Read the redux documentation",
+        completed: false,
+    },
+
+    {
+        id: uuid(),
+        value: "Participate in the event to kick polio out of the country",
+        completed: false,
+    },
+];
+
+export default todos;
