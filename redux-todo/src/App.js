e530acacdeea5065ce6b42e2c695d71475af6eba
Hoxtygen <hoxtygen@live.com>
2019-06-18 19:02:59 +0100

    Persist data in localStorage
    The todos are not saved in localStorage, and the redux store has been modified to get default state from the localStorage.

--- redux-todo/src/App.js
+++ redux-todo/src/App.js
@@ -5,9 +5,11 @@ import { Provider } from "react-redux";
 import todoReducer from "./reducers/todoReducer";
 import TodoList from './components/TodoList';
 import TodoForm from './components/TodoForm';
+import { loadState, saveState} from "./localStorage/localStorage";
 
 
 function App() {
+  const persistedState = loadState()
   const combinedReducer = combineReducers({
     todos: todoReducer,
     
@@ -15,8 +17,14 @@ function App() {
 
   const store = createStore(
     combinedReducer,
+    persistedState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    )
+    );
+    store.subscribe(() => {
+      saveState({
+        todos: store.getState().todos
+      })
+    })
   return (
     <Provider store = {store}>
       <div className="App">
